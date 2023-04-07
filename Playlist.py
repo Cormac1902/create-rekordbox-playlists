1a2e98dd24a5daaac65982cefd16304f74b4199a
Cormac Geraghty <CormacGeraghty@outlook.ie>
2023-04-07 20:17:33 +0100

    Fix bug in playlist_entries instantiation

--- Playlist.py
+++ Playlist.py
@@ -4,8 +4,9 @@ from PlaylistEntry import PlaylistEntry
 
 
 class Playlist:
-    playlist_entries: List[PlaylistEntry] = []
+    playlist_entries: List[PlaylistEntry]
 
     def __init__(self, title, filepath):
         self.title: str = title
         self.filepath: str = filepath
+        self.playlist_entries = []
