7ca9707d1ac41ffb119f674b33ff7c84324c3769
Cormac Geraghty <CormacGeraghty@outlook.ie>
2024-09-04 00:23:58 +0100

    Add further PlaylistEntryTests
    Rename MediaInfoMetadata to MediaInfoAdapter
    
    Refactors to PlaylistEntry, PlaylistEntryMetadata, and MediaInfoAdapter to lazy-initialise data

--- playlist_creator/playlist_parser/MediaInfoAdapter.py
+++ playlist_creator/playlist_parser/MediaInfoAdapter.py
@@ -25,41 +25,40 @@ def get_tag(tags, tag):
     return tags.get(tag) or tags.get(tag.upper())
 
 
-def _try_run(cmd) -> str:
-    try:
-        output_bytes = subprocess.check_output(cmd, shell=True)
-    except subprocess.CalledProcessError:
-        return ''
-
-    return output_bytes.decode('utf-8')
-
-
-class MediaInfoMetadata(MediaInfo):
-    _metadata: dict
+class MediaInfoAdapter(MediaInfo):
+    _metadata: dict = None
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._metadata = dict()
-        self._load_metadata()
 
     def contains_metadata(self) -> bool:
-        return len(self._metadata) > 0
+        return len(self.get_metadata()) > 0
 
-    def get(self, key) -> str:
-        if key in self._metadata:
-            return self._metadata.get(key)
+    def get(self, key) -> str | None:
+        metadata = self.get_metadata()
 
-        print(f"{self.filename}'s metadata does not contain the {key} tag", file=sys.stderr, flush=True)
-        return ''
+        if key in metadata:
+            return metadata.get(key)
 
-    def _load_metadata(self):
+        if self.filename:
+            print(f"{self.filename}'s metadata does not contain the {key} tag", file=sys.stderr, flush=True)
+
+        return None
+
+    def get_metadata(self) -> dict:
+        if not self._metadata:
+            self._metadata = self._load_metadata()
+
+        return self._metadata
+
+    def _load_metadata(self) -> dict:
         if not os.path.exists(self.filename) or not os.path.exists(self.cmd):
-            return None
+            return dict()
 
         cmd_name = os.path.basename(self.cmd)
 
         if cmd_name == 'ffprobe' or 'ffprobe.exe':
-            self._metadata = self._ffprobe_get_metadata()
+            return self._ffprobe_get_metadata()
 
     def _ffprobe_get_metadata(self) -> dict:
         ffprobe_output = self._run_ffprobe()
@@ -76,6 +75,15 @@ class MediaInfoMetadata(MediaInfo):
         return metadata
 
     def _run_ffprobe(self) -> str:
-        return _try_run(
+        return self._try_run(
             f'"{self.cmd}" -loglevel quiet -print_format json -show_format -show_error -i "{self.filename}"'
         )
+
+    def _try_run(self, cmd) -> str:
+        try:
+            output_bytes = subprocess.check_output(cmd, shell=True)
+        except subprocess.CalledProcessError:
+            print(f"Failed to call FFProbe for: {self.filename}", file=sys.stderr, flush=True)
+            return ''
+
+        return output_bytes.decode('utf-8')
