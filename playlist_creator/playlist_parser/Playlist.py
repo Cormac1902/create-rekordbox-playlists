02623c3fc0470f1206598b35f5a5307093f5fe91
Cormac Geraghty <CormacGeraghty@outlook.ie>
2024-09-05 16:42:22 +0100

    Declare playlist_entries in constructor

--- playlist_creator/playlist_parser/Playlist.py
+++ playlist_creator/playlist_parser/Playlist.py
@@ -2,9 +2,7 @@ from .PlaylistEntry import PlaylistEntry
 
 
 class Playlist:
-    playlist_entries: dict[PlaylistEntry]
-
     def __init__(self, title, filepath):
         self.title: str = title
         self.filepath: str = filepath
-        self.playlist_entries = dict()
+        self.playlist_entries: dict[PlaylistEntry] = dict[PlaylistEntry]()
