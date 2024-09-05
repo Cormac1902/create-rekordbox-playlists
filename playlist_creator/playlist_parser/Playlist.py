1fa389818d0f7cb3d008fad53a998a6b1ccd6344
Cormac Geraghty <CormacGeraghty@outlook.ie>
2024-09-05 17:20:42 +0100

    Make Playlist a dataclass

--- playlist_creator/playlist_parser/Playlist.py
+++ playlist_creator/playlist_parser/Playlist.py
@@ -2,7 +2,7 @@ from .PlaylistEntry import PlaylistEntry
 
 
 class Playlist:
-    def __init__(self, title, filepath):
+    def __init__(self, title = None, filepath = None):
         self.title: str = title
         self.filepath: str = filepath
         self.playlist_entries: dict[PlaylistEntry] = dict[PlaylistEntry]()
